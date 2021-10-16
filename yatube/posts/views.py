19f6e54f6ae4ab6b9c3cf06a897e3a431a28985e
Nataly <k1j21@yandex.ru>
2021-10-16 05:47:53 +0300

    ill do it tomorrow

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import cache_page
+from django.core.cache import cache_page
 
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render, redirect
